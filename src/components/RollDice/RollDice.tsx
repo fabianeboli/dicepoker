a561a692f3de24794189451cba64ec6e0b4f648d
Fabian <fabiane@disroot.org>
2019-07-11 16:53:37 +0200

    Changed into two player's hands

--- src/components/RollDice/RollDice.tsx
+++ src/components/RollDice/RollDice.tsx
@@ -1,14 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styles from './RollDice.module.sass'
 import { tsConstructorType } from '@babel/types';
 import { faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix, IconDefinition } from '@fortawesome/free-solid-svg-icons'
 import { number } from 'prop-types';
 import { Icon } from '@fortawesome/fontawesome-svg-core';
 import { Die } from '../Die/Die';
-
-interface IProps {
-    die?: die 
-}
 
 enum buttonText {
     idle = 'Roll dices',
@@ -18,121 +14,78 @@ enum buttonText {
 interface State {
     plDices: die[],
     enDices: die[],
-    diceOne: die,
-    diceTwo: die,
     buttonText: buttonText,
     dicesStyle: string,
     round: number
 }
 
 type die = {
-    number: number, 
+    number: number,
     icon: IconDefinition
 }
 
-const dicesList: die[] = [{number: 1, icon: faDiceOne},{number: 2, icon: faDiceTwo}, {number:3, icon: faDiceThree},
-              {number: 4, icon: faDiceFour},{number: 5, icon: faDiceFive},{number: 6, icon: faDiceSix}]
-
-// export class RollDice extends React.Component<IProps,State> {
-   
-
-//     constructor(props: IProps){
-//         super(props)
-//         this.state = {
-//             plDices: [],
-//             enDices: [],
-//             diceOne: dicesList[0],
-//             diceTwo: dicesList[5],
-//             buttonText: buttonText.idle,
-//             dicesStyle: styles.dices,
-//             round: 1 
-//         }
-//     }
-
-//     private rollSet = (st: State) => {
-//         const randomDice = () => Math.floor(Math.random() * dicesList.length)
-//         return {
-//             ...st,
-//             diceOne: dicesList[randomDice()], 
-//             diceTwo: dicesList[randomDice()], 
-//             buttonText: buttonText.idle,
-//             dicesStyle:  `${styles.dices}`
-//         }
-//     }
-
-//     private rollingAnimation = () => {
-//         return {
-//             buttonText: buttonText.rolling, 
-//             dicesStyle: `${styles.dices} ${styles.rollingAnimation}`
-//         }
-//     }
-
-//     private rollDice = () => {
-//         this.setState(this.rollingAnimation)
-//         setTimeout(() => this.setState(this.rollSet), 200)
-        
-//     }
-
-//     private presentDices = () => {}
-
-//     render() {
-//         return (
-//             <>
-//             <div className={this.state.dicesStyle}>
-//                <Die numberOfDies={this.state.diceOne.icon}/>
-//                <Die numberOfDies={this.state.diceTwo.icon}/>
-//             </div>
-//             <button className={styles.rollButton} onClick={() => this.rollDice()}>{this.state.buttonText}</button>
-//             </>
-//         )
-//     }
-   
-// }
+const dicesList: die[] = [{ number: 1, icon: faDiceOne }, { number: 2, icon: faDiceTwo }, { number: 3, icon: faDiceThree },
+{ number: 4, icon: faDiceFour }, { number: 5, icon: faDiceFive }, { number: 6, icon: faDiceSix }]
 
 
 export const RollDice = () => {
-    const [state, setState] = useState<State>({ plDices: [],
-        enDices: [],
-        diceOne: dicesList[0],
-        diceTwo: dicesList[5],
+    const randomDie = () => Math.floor(Math.random() * dicesList.length)
+
+    const randomizeDicesSet = (dices: die[], numberOfDies: number = dices.length): die[] => {
+        for (let i = 0; i < numberOfDies; i++) {
+            const rndDie = randomDie()
+            const swp = dices[rndDie]
+            dices[rndDie] = dices[i]
+            dices[i] = swp
+        }
+        return dices
+    }
+
+    const [state, setState] = useState<State>({
+        plDices: randomizeDicesSet(dicesList),
+        enDices: randomizeDicesSet(dicesList),
         buttonText: buttonText.idle,
         dicesStyle: styles.dices,
-        round: 1 })
+        round: 1
+    })
 
-        const rollSet = (st: State) => {
-            const randomDice = () => Math.floor(Math.random() * dicesList.length)
-            return {
-                ...st,
-                diceOne: dicesList[randomDice()], 
-                diceTwo: dicesList[randomDice()], 
-                buttonText: buttonText.idle,
-                dicesStyle:  `${styles.dices}`
-            }
+    const rollSet = (st: State) => {
+        return {
+            ...st,
+            buttonText: buttonText.idle,
+            dicesStyle: `${styles.dices}`
         }
-    
-        const rollingAnimation = (st: State) => {
-            return {
-                ...st,
-                buttonText: buttonText.rolling, 
-                dicesStyle: `${styles.dices} ${styles.rollingAnimation}`
-            }
-        }
-    
-        const rollDice = () => {
-            setState(rollingAnimation)
-            setTimeout(() => setState(rollSet), 200)
-            
-        }
-    
-        const presentDices = () => {}
+    }
 
-        return (
-            <>
-            <div className={state.dicesStyle}>
-               <Die numberOfDies={state.diceOne.icon}/>
-               <Die numberOfDies={state.diceTwo.icon}/>
+    const rollingAnimation = (st: State) => {
+        return {
+            ...st,
+            buttonText: buttonText.rolling,
+            dicesStyle: `${styles.dices} ${styles.rollingAnimation}`
+        }
+    }
+
+    const rollDice = () => {
+        setState(rollingAnimation)
+        setTimeout(() => setState(rollSet), 200)
+
+    }
+
+    const presentDices = (diceSet: die[]) => diceSet.map(el => <Die numberOfDies={el.icon} />)
+
+    return (
+        <>
+            <div >
+                <div > 
+                    <h3 className={styles.handName}>Enemy's set</h3>
+               <div className={state.dicesStyle}> {presentDices(state.enDices)} </div>
+                </div>
+                <div >
+                    <h3 className={styles.handName}>Player's set</h3>
+               <div className={state.dicesStyle}> {presentDices(state.plDices)} </div>
+                </div>
             </div>
             <button className={styles.rollButton} onClick={() => rollDice()}>{state.buttonText}</button>
-            </>
-        )
+        </>
+    )
 }
